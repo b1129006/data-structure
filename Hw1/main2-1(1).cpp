7c0fcd9fffd6ceacb70d5db8d51880f9c6fa8abc
b1129006 <120005503+b1129006@users.noreply.github.com>
2023-09-25 00:22:28 +0800

    Add files via upload

---
+++ Hw1/main2-1(1).cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <string.h>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cout << "Number of people:" << endl;
+    cin >> x;
+    cout << "The maximun length of the name: " << endl;
+    cin >> y;
+    cin.get(); // cin.get 吃掉換行
+    char a[y+1];
+    char **p = new char*[x];
+    int max = 0;
+    int *pBlankPos = new int [x];
+    int maxBlankPos = -1;
+    
+    for (int i = 0; i < x; i++) {
+        cout << "Enter a name: ";
+        //p[i] = new char[y+1];
+        cin.getline(p[i] = new char [y + 1], y+1);
+        //strcpy(p[i], a);
+        cout << p[i] << endl;
+        
+        int j = 0;
+        while(p[i][j] != ' ' && p[i][j] != 0)   j++;
+        cout << "j = " << j << endl;
+        pBlankPos [i] = j;
+        if (j > maxBlankPos) {
+            maxBlankPos = j;
+        }
+        cout << "maxBlankPos: " << maxBlankPos << endl; //test
+    }
+    
+    for (int k = 0; k < x; k++){
+        //cout << pBlankPos[k] << " "; //test
+        for (int h = pBlankPos[k]; h < maxBlankPos; h++){
+            cout << " ";
+        }
+        for (int m = 0; m < y; m++){
+            cout << p[k][m];
+        }
+        cout << "\n";
+    }
+    
+    /*
+    for (int k = 0; k < x; k++){
+        for (int l = 0; l < y; l++){
+            if(p[k][l] == ' '){
+                
+            }
+        }
+    }
+    
+    
+    
+    for (int m = 0; m < x; m++) {
+        for (int n = 0; n < y; n++){
+            cout << p[m][n];
+        }
+        cout << "\n";
+    }
+    return 0;
+    
+    delete p;
+    */
+}
